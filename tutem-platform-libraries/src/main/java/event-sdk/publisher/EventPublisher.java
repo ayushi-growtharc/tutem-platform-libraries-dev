031f3b6361039060acc2773604f606bae922c0d3
Ayushi Jain <ayushi.jain@growtharc.com>
2026-07-30 14:26:09 +0530

    Initial set up

---
+++ tutem-platform-libraries/src/main/java/event-sdk/publisher/EventPublisher.java
@@ -0,0 +1,9 @@
+package com.tutem.platform.eventsdk.publisher;
+
+import com.tutem.platform.eventsdk.model.DomainEvent;
+
+public class EventPublisher {
+    void  publish() {
+       DomainEvent event;
+    }
+}
